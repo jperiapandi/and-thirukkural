7dfab27219926ccf270e2f223b7530df2752e1fb
Periapandi Jeyaram <periapandi.jeyaram@anthem.com>
2017-07-25 19:01:54 -0500

    Basic settings view fragment is created

--- app/src/main/java/com/jpp/and_thirukkural/SettingsActivity.java
+++ app/src/main/java/com/jpp/and_thirukkural/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.jpp.and_thirukkural;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,11 +16,24 @@ public class SettingsActivity extends ThirukkuralBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        applyFontForToolbarTitle(toolbar);
 
+        applyFontForToolbarTitle(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getFragmentManager().
+                beginTransaction().
+                replace(android.R.id.content, new ThirukkuralPreferenceFragment()).
+                commit();
     }
 
+    public static class ThirukkuralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.app_preference);
+        }
+    }
 }
