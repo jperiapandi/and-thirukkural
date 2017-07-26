23b4adbf5ca49027a83d5572a4d92c84e9f14430
Periapandi Jeyaram <periapandi.jeyaram@anthem.com>
2017-07-25 20:00:36 -0500

    Settings content alignment issue fixed

--- app/src/main/java/com/jpp/and_thirukkural/SettingsActivity.java
+++ app/src/main/java/com/jpp/and_thirukkural/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.jpp.and_thirukkural;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -8,7 +9,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.ListView;
 
 public class SettingsActivity extends ThirukkuralBaseActivity {
 
@@ -25,10 +29,24 @@ public class SettingsActivity extends ThirukkuralBaseActivity {
 
         getFragmentManager().
                 beginTransaction().
-                replace(android.R.id.content, new ThirukkuralPreferenceFragment()).
+                replace(R.id.frameLayout, new ThirukkuralPreferenceFragment()).
+//                replace(android.R.id.content, new ThirukkuralPreferenceFragment()).
                 commit();
     }
+/*
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        int horizontalMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
+        int verticalMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
+        int topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (int) getResources().getDimension(R.dimen.activity_vertical_margin) + 30, getResources().getDisplayMetrics());
 
+        View view = findViewById(android.R.id.content);
+        view.setPadding(horizontalMargin, topMargin, horizontalMargin, verticalMargin);
+
+        return super.onCreateView(name, context, attrs);
+
+    }
+*/
     public static class ThirukkuralPreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
