9d351479fb0f79d6fab8f1df6296469ee1482e75
jperiapandi <jperiapandi@gmail.com>
2020-10-03 09:15:17 -0400

    Upgraded targetSDK version to 30 Android 11

--- app/src/main/java/com/jpp/and/thirukkural/SettingsActivity.java
+++ app/src/main/java/com/jpp/and/thirukkural/SettingsActivity.java
@@ -2,12 +2,22 @@ package com.jpp.and.thirukkural;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Objects;
 
 public class SettingsActivity extends ThirukkuralBaseActivity {
+    public static final String TAG = "ThirukkuralAPP";
     public static final String KEY_COMM_1 = "commentary1";
     public static final String KEY_COMM_2 = "commentary2";
     public static final String KEY_COMM_3 = "commentary3";
@@ -16,6 +26,7 @@ public class SettingsActivity extends ThirukkuralBaseActivity {
     public static final String KEY_MORNING_COUPLET = "morningCouplet";
     public static final String KEY_EVENING_COUPLET = "eveningCouplet";
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +41,23 @@ public class SettingsActivity extends ThirukkuralBaseActivity {
                 beginTransaction().
                 replace(R.id.frameLayout, new ThirukkuralPreferenceFragment()).
                 commit();
+        //
+        this.db.collection("preference")
+                .get()
+                .addOnCompleteListener(
+                        new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                    for(QueryDocumentSnapshot doc:task.getResult()){
+                                        Log.d(TAG, doc.getId()+" => "+doc.getData());
+                                    }
+                                }else{
+                                    Log.w(TAG, "Error getting preference", task.getException());
+                                }
+                            }
+                        }
+                );
     }
 
     public static class ThirukkuralPreferenceFragment extends PreferenceFragment {
